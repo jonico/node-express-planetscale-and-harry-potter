bb810c31a6da9ec21f17b763543f4e080a9add30
Johannes Nicolai <jonico@planetscale.com>
2022-03-22 10:35:45 +0000

    s/planetscale-demos/planetscale-demo/

--- .pscale/cli-helper-scripts/set-db-and-org-and-branch-name.sh
+++ .pscale/cli-helper-scripts/set-db-and-org-and-branch-name.sh
@@ -11,9 +11,9 @@ if [ -z "${ORG_NAME}" ]; then
         echo "Error: Failed to get PlanetScale org name, please set ORG_NAME explicitly or use Web based SSO, service tokens do not allow to list orgs."
         exit 1
     fi
-    # if org name is set to planetscale, we will set org name to planetscale-demos instead
+    # if org name is set to planetscale, we will set org name to planetscale-demo instead
     if [ "${ORG_NAME}" = "planetscale" ]; then
-        export ORG_NAME="planetscale-demos"
+        export ORG_NAME="planetscale-demo"
     fi
 fi
 
